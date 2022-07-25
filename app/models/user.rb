26ac5b3854e1875d6a9de5a7027b2c66d8c60de9
takara1356 <goblin7283@gmail.com>
2022-07-25 23:31:56 +0900

    ユーザー関連の処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,27 @@
 class User < ApplicationRecord
+  # TODO: パラメータのバリデーション
   has_secure_password
+
+  RegistrationRewardPoint = 10000
+
+  def self.create(params)
+    valid_password(params[:password], params[:password_confirmation])
+    # TODO: 例外処理の追加
+    ApplicationRecord.transaction do
+      user = create!(name: params[:name],
+              email: params[:email],
+              password: params[:password],
+              password_confirmation: params[:password_confirmation],
+              point: RegistrationRewardPoint)
+      user
+    end
+  end
+
+  private
+
+  def self.valid_password(password, password_confirmation)
+    if password != password_confirmation
+      raise "パスワードが一致しません"
+    end
+  end
 end
